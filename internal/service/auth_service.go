84c9ccfa9375eb42e47311156bcd3b6e97ee38ee
kamlesh kumar <coderkamlesg68@gmail.com>
2025-12-13 01:06:52 +0530

    disabled otp wait time

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -236,11 +236,11 @@ func (s *authService) sendLoginOTP(mobile string, allowedRoles ...constants.Role
 
 	if existingOtp != nil {
 		// Check rate limiting
-		secondsSinceLastOtp := int(now.Sub(existingOtp.CreatedAt).Seconds())
-		if secondsSinceLastOtp < OTP_RATE_LIMIT_SECONDS {
-			secondsLeft := OTP_RATE_LIMIT_SECONDS - secondsSinceLastOtp
-			return fmt.Errorf("please wait %d seconds before requesting a new OTP", secondsLeft)
-		}
+		// secondsSinceLastOtp := int(now.Sub(existingOtp.CreatedAt).Seconds())
+		// if secondsSinceLastOtp < OTP_RATE_LIMIT_SECONDS {
+		// 	secondsLeft := OTP_RATE_LIMIT_SECONDS - secondsSinceLastOtp
+		// 	return fmt.Errorf("please wait %d seconds before requesting a new OTP", secondsLeft)
+		// }
 
 		// Update existing OTP
 		existingOtp.OTP = otpValue
